d51b46f670170e95833684700496a747578df3f6
JenSen17376182703 <3583821469@qq.com>
2024-12-17 19:53:19 +0800

    完整后台代码

---
+++ eclipse/ForestBlog-master/ForestBlog/src/main/java/com/niit/service/LinkService.java
@@ -0,0 +1,62 @@
+package com.niit.service;
+
+import com.niit.entity.Link;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ *
+ * @author 言曌
+ * @date 2017/9/4
+ */
+
+public interface LinkService {
+
+    /**
+     * 获得链接总数
+     * 
+     * @param status 状态
+     * @return 数量
+     */
+    Integer countLink(Integer status);
+
+    /**
+     * 获得链接列表
+     * 
+     * @param status 状态
+     * @return 链接列表
+     */
+    List<Link> listLink(Integer status);
+
+    /**
+     * 添加链接
+     * 
+     * @param link 链接
+     */
+    void insertLink(Link link);
+
+    /**
+     * 删除链接
+     * 
+     * @param id 链接ID
+     */
+    void deleteLink(Integer id);
+
+    /**
+     * 更新链接
+     * 
+     * @param link 链接
+     */
+    void updateLink(Link link);
+
+    /**
+     * 根据id查询链接
+     * 
+     * @param id 链接ID
+     * @return 链接
+     */
+    Link getLinkById(Integer id);
+
+
+}
